4d921738d9970da2dd3f47e04204e1c18a7fc393
kimste2 <steven_kim@student.uml.edu>
2018-01-13 20:03:27 -0500

    completed program

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -2,9 +2,31 @@
 int main(int argc, char* argv[])
 {
   int count;
+  std::cout << "Fizz buzz. Print the first hundred numbers if the number is divisable by 3 print fizz, if the number is divisable by 5 print buzz.  If divisible by both print fizz buzz." << std:: endl; 
   for(count = 1; count < 101; count++)
   {
-    std::cout << count << std::endl;
+	if(count % 5 == 0 && count % 3 == 0)
+	{
+
+		std:: cout << "Fizz Buzz" << std:: endl;
+	}
+	else if(count % 5 == 0)
+	{
+		std:: cout << "Fizz\n";
+
+
+	}
+	else if(count % 3 == 0)
+	{
+
+		std:: cout << "Buzz\n";
+	}
+	else
+	{
+		std:: cout <<count << std:: endl; 
+	}
+	
+
   }
  
   return 0;
